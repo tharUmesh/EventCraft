abd4dd70c05b109f21f5c75e07799a5af16fb912
Tharun-Umesh <tharunumesh1235@gmail.com>
2025-12-30 15:39:26 +0530

    Make photo field optional

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   firstName: { type: String, default: '' },
   lastName: { type: String, default: '' },
-  photo: { type: String, required: true },
+  photo: { type: String, default: '' },
 })
 
 const User = models.User || model('User', UserSchema);
