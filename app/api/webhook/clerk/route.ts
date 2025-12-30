a5aab8caab56c3d7ed69fc2caa4dfc8bd6e0e695
Tharun-Umesh <tharunumesh1235@gmail.com>
2025-12-30 15:26:31 +0530

    Trigger fresh build

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -71,7 +71,7 @@ export async function POST(req: Request) {
 
     const newUser = await createUser(user);
 
-    console.log('User created:', newUser);
+    console.log('User created in MongoDB:', newUser);
 
     if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
