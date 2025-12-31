141bf7494f4b5dbbd4e410bc04c5fca85d9af5fe
Tharun-Umesh <tharunumesh1235@gmail.com>
2025-12-31 07:58:38 +0530

    fix auth import error

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,6 +1,6 @@
 import EventForm from "@/components/shared/EventForm"
 import { getEventById } from "@/lib/actions/event.actions"
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 type UpdateEventProps = {
   params: {
