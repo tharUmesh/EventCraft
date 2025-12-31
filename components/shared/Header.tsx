b66c1765e8f53f69dbb660e503cfcbc11c4184b5
Tharun-Umesh <tharunumesh1235@gmail.com>
2025-12-31 16:55:21 +0530

    add login button background color

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
             <MobileNav />
           </SignedIn>
           <SignedOut>
-            <Button asChild className="rounded-full" size="lg">
+            <Button asChild className="rounded-full button" size="lg">
               <Link href="/sign-in">
                 Login
               </Link>
